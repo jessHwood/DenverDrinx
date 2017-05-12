d13d1c2dd8cb90c9a1131d624fbd48e38fc41588
jessHwood <jhorntvedt@gmail.com>
2017-05-12 15:59:18 -0600

    add cors help to route

--- server/config/routes.js
+++ server/config/routes.js
@@ -12,4 +12,17 @@ router.get('/', function(req, res){
 //bar routes
 router.get('/api/bars/index', barController.index);
 
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+     // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+});
+
 module.exports = router;
