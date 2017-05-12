03c278d6b23439033f06dfda4f4cc070282036b8
jessHwood <jhorntvedt@gmail.com>
2017-05-12 14:06:11 -0600

    added heroku database

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,19 @@
 //Connect
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://jesswood@localhost:5432/denverdrinx');
+// var sequelize = new Sequelize('postgres://jesswood@localhost:5432/denverdrinx');
 
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  });
+} else {
+  // the application is executed on the local machine
+  sequelize = new Sequelize('postgres://jesswood@localhost:5432/denverdrinx');
+}
 
 //Export models and Sequelize for seed and dbSetup
 module.exports.Sequelize = Sequelize;
